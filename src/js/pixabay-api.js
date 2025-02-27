e49ed2a698b805d72063f9a8cb333722b48c12bf
HannaOs-GOIT <bestkoss@ukr.net>
2025-02-27 11:31:21 +0200

    HW11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '49053882-7e244883c6f1c912e1433ba1a';
+
+export async function fetchImages(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
+
+
+
---
+++ src/js/render-functions.js
@@ -0,0 +1,53 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+let lightbox;
+
+
+export function renderImages(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+            <li class="gallery-item">
+                <div class="gallery">
+                    <a href="${largeImageURL}">
+                        <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy">
+                    </a>
+                </div>
+
+                <div class="info">
+                    <p><b>Likes:</b> ${likes}</p>
+                    <p><b>Views:</b> ${views}</p>
+                    <p><b>Comments:</b> ${comments}</p>
+                    <p><b>Downloads:</b> ${downloads}</p>
+                </div>
+            </li>
+        `
+    )
+    .join('');
+
+  gallery.innerHTML = markup;
+
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  }
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+  if (lightbox) {
+    lightbox.destroy();
+    lightbox = null;
+  }
+}
+
