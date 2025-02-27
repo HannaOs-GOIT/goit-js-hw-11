318942d6dee62d9ff3e90e419b71c7ca5944c7b5
HannaOs-GOIT <bestkoss@ukr.net>
2025-02-27 18:21:59 +0200

    HW11

--- src/main.js
+++ src/main.js
@@ -6,52 +6,41 @@ import 'izitoast/dist/css/iziToast.min.css';
 import { fetchImages } from './js/pixabay-api.js';
 import { renderImages, clearGallery } from './js/render-functions.js';
 
-const form = document.getElementById('search-form');
-const loader = document.getElementById('loader');
-const loadingText = document.getElementById('loading-text');
 
-form.addEventListener('submit', handleSubmit);
+const form = document.querySelector('.search-form');
+const loader = document.querySelector('.loader');
+const gallery = document.querySelector('.gallery');
 
-async function handleSubmit(event) {
+form.addEventListener('submit', async event => {
   event.preventDefault();
-
-const query = event.target.elements.searchQuery.value.trim();
-    
+  const query = event.target.elements.searchQuery.value.trim();
 
   if (!query) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Please enter a search query.',
-    });
+    iziToast.warning({ message: 'Please enter a search query!' });
     return;
   }
 
   loader.style.display = 'block';
-  loadingText.style.display = 'block';
-
-  clearGallery();
+  gallery.innerHTML = '';
 
   try {
     const images = await fetchImages(query);
 
     if (images.length === 0) {
-      iziToast.info({
-        title: 'Info',
+      iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
       });
-    } else {
-      renderImages(images);
+      return;
     }
+
+    renderImages(images);
   } catch (error) {
-    console.error('Error fetching images:', error);
     iziToast.error({
-      title: 'Error',
-      message: 'Failed to fetch images. Please try again later.',
+      message: 'Something went wrong. Please try again later.',
     });
   } finally {
     loader.style.display = 'none';
-    loadingText.style.display = 'none';
-    event.target.reset();
   }
-}
+});
+
